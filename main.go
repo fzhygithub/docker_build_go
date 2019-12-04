5a422ab19a41451ece6bbe8bb633c737f169f13c
fzhygithub <6562157@qq.com>
2019-12-04 13:49:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
    "net/http"
 )
 func handlerHello(w http.ResponseWriter, r *http.Request) {
-   fmt.Fprintf(w, "hello Go prod")
+   fmt.Fprintf(w, "master hello Go")
 }
 func main() {
    http.HandleFunc("/", handlerHello)
